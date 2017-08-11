50aad2851453e9dd3c280e256f22d36f24fe8f60
Wang <baohuaw@amazon.com>
2017-08-11 18:37:10 +0800

    bug fixing

--- esp.cpp
+++ esp.cpp
@@ -14,12 +14,13 @@ struct Node {
     int earliestTime;
     int latestTime;
     int demand;
+    double cost;
 
     bool operator<(const Node &other) const {
         return id < other.id;
     }
 
-    bool operator==(const Node& other) {
+    bool operator==(const Node &other) {
         return this == &other;
 
     }
@@ -34,7 +35,7 @@ struct Arc {
 
 struct Label {
     Node node;
-    Label* preLabel;
+    Label *preLabel;
     double cost;
     int arrivalTime;
     int curDemand;
@@ -43,7 +44,7 @@ struct Label {
         return arrivalTime < other.arrivalTime;
     }
 
-    bool operator==(const Label& other) {
+    bool operator==(const Label &other) {
         return this == &other;
 
     }
@@ -55,30 +56,30 @@ struct Label {
                                                                                             curDemand(curDemand) {};
 
 
-    virtual ~Label() {
-        delete preLabel;
-
-    }
+//    virtual ~Label() {
+//        delete preLabel;
+//
+//    }
 
 };
 
-vector<Label*> NPS;
+vector<Label *> NPS;
 
 const int capacity = 10;
 
-map<Node, vector<Label*>> nodeLabels;
+map<Node, vector<Label *>> nodeLabels;
 
 map<Node, vector<Arc>> outArcs;
 
 map<int, Node> nodes;
 
 void init() {
-    nodes[0] =  {0, 0, 0, 0};
-    nodes[1] = {1, 6, 14, 0};
-    nodes[2] = {2, 9, 12, 0};
-    nodes[3] = {3, 8, 12, 0};
-    nodes[4] = {4, 9, 35, 0};
-    nodes[5] = {5, 15, 25, 0};
+    nodes[0] = {0, 0, 0, 0, 0};
+    nodes[1] = {1, 6, 14, 0, 1};
+    nodes[2] = {2, 9, 12, 0, 1};
+    nodes[3] = {3, 8, 12, 0, 2};
+    nodes[4] = {4, 9, 35, 0, 1};
+    nodes[5] = {5, 15, 25, 0, 1};
 
     outArcs[nodes[0]].push_back({nodes[0], nodes[1], 3, 8});
     outArcs[nodes[0]].push_back({nodes[0], nodes[2], 5, 5});
@@ -95,41 +96,42 @@ void init() {
 }
 
 void shortestPath(Node &source) {
-    Label* sourceLabel = new Label(source, nullptr, 0, 0,0);
+    auto *sourceLabel = new Label(source, nullptr, 0, 0, 0);
     nodeLabels[source].push_back(sourceLabel);
     NPS.push_back(sourceLabel);
 
     while (!NPS.empty()) {
         sort(NPS.begin(), NPS.end());
-        Label* targetLabel = NPS[0];
+        Label *targetLabel = NPS[0];
         NPS.erase(NPS.begin());
 
         for (auto &arc : outArcs[targetLabel->node]) {
             if (arc.target.id != targetLabel->node.id &&
                 targetLabel->arrivalTime + arc.time <= arc.target.latestTime &&
                 targetLabel->curDemand + arc.target.demand <= capacity) {
-                if (nodeLabels.count(arc.target) == 0 || (nodeLabels.count(arc.target) != 0 && nodeLabels[arc.target].empty())) {
-                    Label* newLabel = new Label(arc.target, targetLabel, targetLabel->cost + arc.cost,
-                                      max(targetLabel->arrivalTime + arc.time, arc.target.earliestTime),
-                                      targetLabel->curDemand + arc.target.demand);
+                if (nodeLabels.count(arc.target) == 0 ||
+                    (nodeLabels.count(arc.target) != 0 && nodeLabels[arc.target].empty())) {
+                    Label *newLabel = new Label(arc.target, targetLabel, targetLabel->cost + arc.cost + arc.target.cost,
+                                                max(targetLabel->arrivalTime + arc.time, arc.target.earliestTime),
+                                                targetLabel->curDemand + arc.target.demand);
                     nodeLabels[arc.target].push_back(newLabel);
                     NPS.push_back(newLabel);
                 } else {
                     bool addNewLabel = true;
                     for (auto it = nodeLabels[arc.target].begin(); it != nodeLabels[arc.target].end();) {
 
-                        if ((*it)->cost >= targetLabel->cost + arc.cost &&
-                                (*it)->arrivalTime >= targetLabel->arrivalTime + arc.time &&
-                                (*it)->curDemand >= targetLabel->curDemand + arc.target.demand) {
+                        if ((*it)->cost >= targetLabel->cost + arc.cost + arc.target.cost &&
+                            (*it)->arrivalTime >= targetLabel->arrivalTime + arc.time &&
+                            (*it)->curDemand >= targetLabel->curDemand + arc.target.demand) {
 
-                            for (auto it2 = NPS.begin(); it2 != NPS.end(); ) {
-                                if((*it2)->node.id == (*it)->node.id &&
-                                        (*it2)->preLabel == (*it)->preLabel &&
-                                        (*it2)->cost == (*it)->cost &&
-                                        (*it2)->arrivalTime == (*it)->arrivalTime &&
-                                        (*it2)->curDemand == (*it)->curDemand){
+                            for (auto it2 = NPS.begin(); it2 != NPS.end();) {
+                                if ((*it2)->node.id == (*it)->node.id &&
+                                    (*it2)->preLabel == (*it)->preLabel &&
+                                    (*it2)->cost == (*it)->cost &&
+                                    (*it2)->arrivalTime == (*it)->arrivalTime &&
+                                    (*it2)->curDemand == (*it)->curDemand) {
                                     it2 = NPS.erase(it2);
-                                }else{
+                                } else {
                                     ++it2;
                                 }
                             }
@@ -138,9 +140,9 @@ void shortestPath(Node &source) {
                             it = nodeLabels[arc.target].erase(it);
 
                         } else {
-                            if ((*it)->cost <= targetLabel->cost + arc.cost &&
-                                    (*it)->arrivalTime <= targetLabel->arrivalTime + arc.time &&
-                                    (*it)->curDemand <= targetLabel->curDemand + arc.target.demand) {
+                            if ((*it)->cost <= targetLabel->cost + arc.cost + arc.target.cost &&
+                                (*it)->arrivalTime <= targetLabel->arrivalTime + arc.time &&
+                                (*it)->curDemand <= targetLabel->curDemand + arc.target.demand) {
                                 addNewLabel = false;
                                 break;
                             }
@@ -150,9 +152,10 @@ void shortestPath(Node &source) {
                     }
 
                     if (addNewLabel) {
-                        Label* newLabel = new Label(arc.target, targetLabel, targetLabel->cost + arc.cost,
-                                          max(targetLabel->arrivalTime + arc.time, arc.target.earliestTime),
-                                          targetLabel->curDemand + arc.target.demand);
+                        Label *newLabel = new Label(arc.target, targetLabel,
+                                                    targetLabel->cost + arc.cost + arc.target.cost,
+                                                    max(targetLabel->arrivalTime + arc.time, arc.target.earliestTime),
+                                                    targetLabel->curDemand + arc.target.demand);
                         nodeLabels[arc.target].push_back(newLabel);
                         NPS.push_back(newLabel);
                     }
@@ -165,7 +168,7 @@ void shortestPath(Node &source) {
 
     for (auto &node : nodeLabels) {
         for (auto &label : node.second) {
-            Label* targetLabel = label;
+            Label *targetLabel = label;
             if (targetLabel->arrivalTime > targetLabel->node.latestTime) {
                 cout << "Node " << targetLabel->node.id << " violate time window" << endl;
                 return;
@@ -181,13 +184,13 @@ void shortestPath(Node &source) {
                     return;
                 }
             }
-            cout << "Path from " << source.id << " to " << label->node.id << "   ->   " << path << "   cost: "
+            cout << "Path from " << source.id << " to " << label->node.id << "   -> " << path << "   cost: "
                  << label->cost << endl;
         }
     }
 
-    for(auto itr = nodeLabels.begin(); itr != nodeLabels.end(); itr++){
-        for (auto it = itr->second.begin() ; it != itr->second.end(); ++it){
+    for (auto itr = nodeLabels.begin(); itr != nodeLabels.end(); itr++) {
+        for (auto it = itr->second.begin(); it != itr->second.end(); ++it) {
             delete (*it);
         }
         itr->second.clear();
